674399f578a460d1056b486bf082da17816c7b00
Fabian <100328289+FabianUlisesCarrillo@users.noreply.github.com>
2022-07-12 13:42:09 -0700

    Added middleware to handle errors

--- server.js
+++ server.js
@@ -2,10 +2,18 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+const notFound = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
 // Root route
 app.get('/', (req, res) => {
+    throw new Error('error')
     res.send('its ok')
 });
+
+app.use(notFound)
+app.use(errorHandlerMiddleware)
+// app.all('*', notFound)
 // Server Listening
 app.listen(process.env.PORT, () => {
     console.log('I am awake')
