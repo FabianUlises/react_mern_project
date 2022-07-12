dc6886821acb48c3be5a69f7215c8a0360746242
Fabian <100328289+FabianUlisesCarrillo@users.noreply.github.com>
2022-07-12 08:43:23 -0700

    Seperated logo into own functional comonent

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,13 +1,13 @@
-import logo from '../assets/images/logo.svg';
 import main from '../assets/images/main.svg';
 import React from 'react';
-import styled from 'styled-components';
+import { Logo } from '../components';
+import { Wrapper } from '../assets/wrappers/LandingWrapper';
 
 const Landing = () => {
   return (
     <Wrapper>
         <nav>
-            <img src={logo} alt="Jobify" className='logo' />
+          <Logo />
         </nav>
         <div className="container page">
             <div className="info">
@@ -20,41 +20,5 @@ const Landing = () => {
     </Wrapper>
   )
 }
-const Wrapper = styled.div`
-  nav {
-    width: var(--fluid-width);
-    max-width: var(--max-width);
-    margin: 0 auto;
-    height: var(--nav-height);
-    display: flex;
-    align-items: center;
-  }
-  .page {
-    min-height: calc(100vh - var(--nav-height));
-    display: grid;
-    align-items: center;
-    margin-top: -3rem;
-  }
-  h1 {
-    font-weight: 700;
-    span {
-      color: var(--primary-500);
-    }
-  }
-  p {
-    color: var(--grey-600);
-  }
-  .main-img {
-    display: none;
-  }
-  @media screen and (min-width: 992px) {
-    .page {
-      grid-template-columns: repeat(2, 1fr);
-      grid-gap: 3rem;
-    }
-    .main-img {
-      display: block;
-    }
-  }
-`
+
 export default Landing
