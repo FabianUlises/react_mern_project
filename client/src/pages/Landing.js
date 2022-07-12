d300085e67511907757a3b44d8733e7d491bda56
Fabian <100328289+FabianUlisesCarrillo@users.noreply.github.com>
2022-07-12 08:25:59 -0700

    Added styles to landing page

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -29,6 +29,32 @@ const Wrapper = styled.div`
     display: flex;
     align-items: center;
   }
-
+  .page {
+    min-height: calc(100vh - var(--nav-height));
+    display: grid;
+    align-items: center;
+    margin-top: -3rem;
+  }
+  h1 {
+    font-weight: 700;
+    span {
+      color: var(--primary-500);
+    }
+  }
+  p {
+    color: var(--grey-600);
+  }
+  .main-img {
+    display: none;
+  }
+  @media screen and (min-width: 992px) {
+    .page {
+      grid-template-columns: repeat(2, 1fr);
+      grid-gap: 3rem;
+    }
+    .main-img {
+      display: block;
+    }
+  }
 `
 export default Landing
