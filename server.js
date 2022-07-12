9380c796bf94b9fb29c0f9a2170d708003a2e939
Fabian <100328289+FabianUlisesCarrillo@users.noreply.github.com>
2022-07-12 13:53:14 -0700

    Installed mongoose

--- server.js
+++ server.js
@@ -4,6 +4,24 @@ const app = express();
 
 const notFound = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
+const connectDB = require('./db/connect')
+
+
+// Function to connect to DB
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(process.env.PORT, () => {
+            console.log('I am awake')
+        });
+    } catch (err) {
+        console.log(err)
+    }
+}
+// Connecting to DB
+start();
+
+
 
 // Root route
 app.get('/', (req, res) => {
@@ -11,10 +29,7 @@ app.get('/', (req, res) => {
     res.send('its ok')
 });
 
+// Error handlers
 app.use(notFound)
 app.use(errorHandlerMiddleware)
 // app.all('*', notFound)
-// Server Listening
-app.listen(process.env.PORT, () => {
-    console.log('I am awake')
-});
