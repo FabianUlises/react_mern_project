5a59ec053daa7721712f4977cc9dffa3fd73954b
Fabian <100328289+FabianUlisesCarrillo@users.noreply.github.com>
2022-07-12 08:12:09 -0700

    Added content to landing oage

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -8,6 +8,14 @@ const Landing = () => {
         <nav>
             <img src={logo} alt="Jobify" className='logo' />
         </nav>
+        <div className="container page">
+            <div className="info">
+                <h1>job <span>tracking</span> app</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam fugit provident voluptate quia pariatur non quisquam suscipit, labore iusto qui.</p>
+                <button className='btn btn-hero'>Login</button>
+            </div>
+            <img src={main} alt="job hunt" className='img main-img' />
+        </div>
     </main>
   )
 }
