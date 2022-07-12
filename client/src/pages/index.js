96664910831c19478a20924927bb58714c6dbea7
Fabian <100328289+FabianUlisesCarrillo@users.noreply.github.com>
2022-07-12 10:07:38 -0700

    Implemented React router

---
+++ client/src/pages/index.js
@@ -0,0 +1,10 @@
+import Dashboard from "./Dashboard";
+import Register from "./Register";
+import Error from "./Error";
+import Landing from "./Landing";
+
+
+export {
+    Dashboard, Register,
+    Error, Landing
+}
